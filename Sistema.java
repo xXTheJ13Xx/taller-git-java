eb1b491ea7744474fa7878bb89d7c4bf06355d73
Jaime <jaimehmc13@gmail.com>
2026-05-30 16:23:10 -0500

    se creo un codigo para  verificar numero pares e impares

--- Sistema.java
+++ Sistema.java
@@ -1,3 +1,22 @@
 public class Sistema {
-    
+    public class NumeroParImpar {
+
+    // Método que verifica si es par o impar
+    public static String verificarNumero(int numero) {
+        if (numero % 2 == 0) {
+            return "El número es PAR";
+        } else {
+            return "El número es IMPAR";
+        }
+    }
+
+    public static void main(String[] args) {
+        int numero = 7; // puedes cambiar el número
+
+        String resultado = verificarNumero(numero);
+
+        System.out.println("Número: " + numero);
+        System.out.println(resultado);
+    }
+}
 }
